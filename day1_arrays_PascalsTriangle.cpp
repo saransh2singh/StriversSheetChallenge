f8e57f77e6089f5a6b370cb6bae986466a88cdbd
saransh2singh <110494720+saransh2singh@users.noreply.github.com>
2023-06-22 17:07:38 +0530

    Update day1_arrays_PascalsTriangle.cpp

--- day1_arrays_PascalsTriangle.cpp
+++ day1_arrays_PascalsTriangle.cpp
@@ -1,22 +1,44 @@
+// class Solution {
+// public:
+    
+//     vector<int>generateRows(int row){
+//         vector<int>ans;
+//         ans.push_back(1);
+//         int result = 1;
+//         for(int i=1;i<row;i++){
+//             raesult = result * (row-i)/i;
+//             ans.push_back(result);
+//         }
+//         return ans;
+//     }
+    
+//     vector<vector<int>> generate(int numRows) {
+//         vector<vector<int>> ans;
+//         for(int i=1;i<=numRows;i++){
+//             ans.push_back(generateRows(i));
+//         }
+//         return ans;
+//     }
+// };
+
+
+// Shorter implementation
 class Solution {
 public:
     
-    vector<int>generateRows(int row){
-        vector<int>ans;
-        ans.push_back(1);
-        int result = 1;
-        for(int i=1;i<row;i++){
-            raesult = result * (row-i)/i;
-            ans.push_back(result);
-        }
-        return ans;
-    }
-    
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans;
-        for(int i=1;i<=numRows;i++){
-            ans.push_back(generateRows(i));
+        vector<vector<int>> pascal(numRows);
+        for(int i = 0; i < numRows; i++)
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                if(j == 0 || j == i) // for first and last row
+                pascal[i].push_back(1);
+
+                else 
+                pascal[i].push_back(pascal[i-1][j-1] + pascal[i-1][j]);
+            }
         }
-        return ans;
+        return pascal; 
     }
 };
