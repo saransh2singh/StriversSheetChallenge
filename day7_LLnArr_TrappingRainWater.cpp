91e2876c8b4568ed5bf50aa139c66d28eb7f15da
Saransh Singh <110494720+saransh2singh@users.noreply.github.com>
2023-07-06 00:00:32 +0530

    Create day7_LLnArr_TrappingRainWater.cpp

---
+++ day7_LLnArr_TrappingRainWater.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0;
+        int left = 0;
+        int right = height.size()-1;
+        int leftbar = height[0];
+        int rightbar = height[height.size()-1];
+        while(left <= right){
+            if(leftbar < rightbar){
+                if(height[left] > leftbar){
+                    leftbar = height[left];
+                }
+                else{
+                    ans += leftbar - height[left];
+                    left++;
+                }
+            }
+            else{
+                if(height[right] > rightbar){
+                    rightbar = height[right];
+                }
+                else{
+                    ans += rightbar - height[right];
+                    right--;
+                }
+            }
+        }
+        return ans;
+    }
+};
