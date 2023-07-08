a7ef443e37394d3724e96d6ef71de50df4659df4
Saransh Singh <110494720+saransh2singh@users.noreply.github.com>
2023-07-08 20:28:27 +0530

    Create day8_Greedy_ActivitySelection.cpp

---
+++ day8_Greedy_ActivitySelection.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+    //Function to find the maximum number of activities that can
+    //be performed by a single person.
+    static bool cmp(pair<int,int>a,pair<int,int>b){
+        return a.second < b.second;
+    }
+    int activitySelection(vector<int> start, vector<int> end, int n)
+    {
+        // Your code here
+        vector<pair <int,int>>v;
+        for(int i=0;i<n;i++){
+            pair<int,int>p = make_pair(start[i],end[i]);
+            v.push_back(p);
+        }
+        sort(v.begin(),v.end(),cmp);
+        int count = 1;
+        int ansEnd = v[0].second;
+        for(int i=1;i<n;i++){
+            if(v[i].first > ansEnd){
+                count++;
+                ansEnd = v[i].second;
+            }
+        }
+        return count;
+    }
+};
